45656b40db003152787c3f09866f816f414991ad
Nico Rogalski (msg) <nico.rogalski@msg-david.de>
2022-06-17 10:32:47 +0200

    health

--- pkg/kit/fiber/health/health.go
+++ pkg/kit/fiber/health/health.go
@@ -1,27 +1,28 @@
 package health
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/rogalni/ms-kit/pkg/kit/health"
+)
 
 type HealthHandler struct {
 	r fiber.Router
 }
 
-type StatusFunc func() Health
+type StatusFunc func() health.Health
 
 func For(app *fiber.App) *HealthHandler {
 	g := app.Group("/health")
-
-	g.Get("", health)
-
+	g.Get("", getHealth)
 	hh := &HealthHandler{
 		r: g,
 	}
 	return hh
 }
 
-func health(app *fiber.Ctx) error {
-	app.JSON(&Health{
-		Status: UP,
+func getHealth(app *fiber.Ctx) error {
+	app.JSON(&health.Health{
+		Status: health.UP,
 	})
 	return nil
 }
@@ -30,8 +31,8 @@ func (h *HealthHandler) WithReadiness(sf StatusFunc) *HealthHandler {
 	h.r.Get("/readiness", func(app *fiber.Ctx) error {
 		es := sf()
 		for _, v := range es.Components {
-			if v.Status == DOWN {
-				es.Status = DOWN
+			if v.Status == health.DOWN {
+				es.Status = health.DOWN
 				break
 			}
 		}
@@ -45,8 +46,8 @@ func (h *HealthHandler) WithLiveness(sf StatusFunc) *HealthHandler {
 	h.r.Get("/liveness", func(app *fiber.Ctx) error {
 		es := sf()
 		for _, v := range es.Components {
-			if v.Status == DOWN {
-				es.Status = DOWN
+			if v.Status == health.DOWN {
+				es.Status = health.DOWN
 				break
 			}
 		}
